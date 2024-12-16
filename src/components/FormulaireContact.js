51d693f86645c3a5e21d64b67b734cd2a2a0131d
Dellon <jy.dellon@gmail.com>
2024-12-16 03:18:33 +0100

    maj

--- src/components/FormulaireContact.js
+++ src/components/FormulaireContact.js
@@ -32,7 +32,7 @@ const FormulaireContact = ({ onClose }) => {
     setIsSubmitting(true);
     setError(null);
     try {
-      const response = await fetch('https://serveraeonixemail.vercel.app/api/contact', {
+      const response = await fetch('https://serveraeonixemail-bdoht364f-dellons-projects.vercel.app/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
